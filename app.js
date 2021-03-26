6bd2a732b03a6625396d1d597afcde32125d5b7a
jesus david ramirez <jesus.ramirez@mipaquete.com>
2021-03-25 22:01:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());   
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
+

8fa21d5096dc7d34e1df403e508c235b4d3bf7a0
jesus david <jesus.ramirez@mipaquete.com>
2021-03-30 23:01:56 -0500

    se arreglo bug de frecuencia en el servidos

--- app.js
+++ app.js
@@ -14,7 +14,6 @@
 
 const { MongoClient, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://platzi-admin:coco04@curso-platzi.5mxnd.mongodb.net/exmaple?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // client.connect( async (err) => {
 //     if (err) process.exit();
 //     console.log('conecto');
@@ -26,6 +25,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //     // perform actions on the collection object
 //     client.close();
 // })
+
+const getClient = () => {
+    try {
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        return client;
+    } catch (e) {
+        throw new Error('connection error');
+    }
+}
+
+const isConnected = async () => {
+    try {
+        const client = await getClient();
+        if (client) {
+            return client;
+        }
+        return undefined;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
 
 var express = require('express')
 var app = express()
@@ -70,13 +90,12 @@ console.log(id);
 app.post('/busqueLoQueQuiera', async (req, res) => {
     const user = req.body;
 
-
-    await client.connect().then(conection => conection).catch(err => {
-        console.log(err);
-        
-    });
-    const collectionUser = await client.db("example").collection("users");
-    const result = await collectionUser.findOne(user);
+    const verifyClient = await isConnected(); // traer sistema de conexion con el closter
+    await verifyClient.connect(); // se genera la conexion con el closter
+    const database = verifyClient.db('example') // verificacion y conexion con la base de datos de mongo db que esta dentro del closter
+    const collectionUser = await database.collection("users");// conexion con la colexion de usuarios que esta dentro de la base de datos
+    const result = await collectionUser.findOne(user);// fenera la operacion dentro de la coleccion de ususarios que en este caso es buscar ususario
+    verifyClient.close(); // cerrar la conexxion con closter y base de datos
     
 
     res.status(200).json(result);

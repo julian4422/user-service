72eb110dbf8bc13790884e965f3f16f27d0dcdc1
jesus david ramirez <jesus.ramirez@mipaquete.com>
2021-04-01 18:41:08 -0500

    clases

---
+++ classes.js
@@ -0,0 +1,24 @@
+// class Person {
+
+//     constructor(name, lastName, age) {
+//         this.name = name;
+//         this.lastName = lastName;
+//         this.age = age;
+//     }
+
+//     aumentarEdad() {
+//         this.age =  this.age + 1;
+//         return this.age;
+//     }
+
+// }
+
+
+// const julian = new Person('julian', 'sanchez', 20);
+// console.log(julian.aumentarEdad());
+
+
+user name username password email
+
+modificar nombre
+cambie el correo

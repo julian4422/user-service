c1e9d3947098b498b5fdd553f5dc1a926e2976eb
jesus david <jesus.ramirez@mipaquete.com>
2021-04-01 19:17:58 -0500

    jejeje

--- classes.js
+++ classes.js
@@ -18,7 +18,29 @@
 // console.log(julian.aumentarEdad());
 
 
-user name username password email
+// user name username password email
 
-modificar nombre
-cambie el correo
+// modificar nombre
+// cambie el correo
+
+class usuario {
+    constructor (username, password, email) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    cambiarNombre(newUsername) {
+        this.username = newUsername;
+    }
+
+    impresion() {
+        console.log(`This is your username: ${this.username},`, `This is your password:  ${this.password},`, `this is your email: ${this.email},`);
+    }
+}
+
+const yisus = new usuario ('drakekaiser', 'coco04', 'yisusChrist@gmail.com')
+// yisus.cambiarNombre('julihermoso')
+// console.log(yisus.username)
+ 
+yisus.impresion();

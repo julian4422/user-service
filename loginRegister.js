374c63d0a6d14e943e8af4b8be99ba4e2e1ba7d5
Julian <julian2000es7@gmail.com>
2021-04-11 18:23:03 -0500

    new login

--- loginRegister.js
+++ loginRegister.js
@@ -1,60 +1,9 @@
-const { MongoClient, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://platzi-admin:coco04@curso-platzi.5mxnd.mongodb.net/exmaple?retryWrites=true&w=majority";
+const router = require('express').Router();
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-// registro, login
-const getClient = () => { // forma sistema de conexion al cluster
-    try {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); // parsea url y la trae en una sola linea
-        return client;
-    } catch (e) {
-        throw new Error('connection error'); // es una clase
-    }
-}
-
-const isConnected = async () => {
-    try {
-        const client = await getClient();
-        if (client) {
-            return client;
-        }
-        return undefined;// si no existe la conexion
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-app.use(bodyParser.json());
-
-/*app.post('/Register', async (req, res) => {
-    const user = req.body;
-
-    const verifyClient = await isConnected();
-    await verifyClient.connect();
-    const database = verifyClient.db('example')
-    const collectionUser = await database.collection("users");
-    const result = await collectionUser.insertOne(user);
-    verifyClient.close();
-    res.json(result);
-});*/
-
-/*app.post('/Login', async (req, res) => {
-    const user = req.body;
-
-    const verifyClient = await isConnected();
-    await verifyClient.connect();
-    const database = verifyClient.db('example')
-    const collectionUser = await database.collection("users");
-    const result = await collectionUser.insertOne(user);
-    verifyClient.close();
-    res.json(result);
-});*/
-
-app.post("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         //Checking if User Exists in dB
         const userExists = await User.findOne({ email: req.body.email });
@@ -87,7 +36,7 @@ app.post("/login", async (req, res) => {
 
 });
 
-app.post("/register", async (req, res) => {
+router.post("/register", async (req, res) => {
     try {
         //Check if Email Already Exists
         const emailExists = await User.findOne({ email: req.body.email });
@@ -126,6 +75,4 @@ app.post("/register", async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
-});
+module.exports = router;
